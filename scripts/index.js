b400f65332f8783204776d4d805943118a99c626
unknown <machel.s.kassah@gmail.com>
2020-07-06 22:03:47 +0000

    completed digital clock

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,44 @@
 'use strict';
 
 window.onload = ()=>{
+    setInterval(()=>{
+
+        let currentDateTime = new Date();
+        let FullDate = currentDateTime.toDateString();
+        let hours = currentDateTime.getUTCHours();
+        // let hours = 20;
+        let minutes = currentDateTime.getUTCMinutes();
+        let seconds = currentDateTime.getUTCSeconds();
+        let fullTime = hours + ":"+ minutes + ":" + seconds;
     
-    setInterval(displayClock, 100);
-}
+        if (hours == 10) {
+            document.getElementById("motivation").innerText = "Its time for Breakfast";
+            document.getElementById("face-background").style.backgroundImage = "url('../images/breakfast.jpg')";
+           
+            
+        }
+        if (hours == 14) {
+            document.getElementById("motivation").innerText = "Its time for Lunch";
+            document.getElementById("face-background").style.backgroundImage = "url('../images/lunch.png')";
+
+            
+        }
+        if (hours == 20) {
+            document.getElementById("motivation").innerText = "Its time for Dinner";
+            document.getElementById("face-background").style.backgroundImage = "url('../images/dinner.jpg')";
+
+            
+        }
+    
+        document.getElementById("clock-date").innerText = FullDate;
+        document.getElementById("clock-time").innerText = fullTime;
+        console.log(hours);
+        // let Day = currentDateTime.getDate();
+        // console.log(Day);
+    
+    }, 100);
+};
 
 
-function displayClock(){
 
-    var currentDateTime = new Date();
-    var FullDate = currentDateTime.toDateString();
-    var hours = currentDateTime.getUTCHours();
-    // var hours = 20;
-    var minutes = currentDateTime.getUTCMinutes();
-    var seconds = currentDateTime.getUTCSeconds();
-    var fullTime = hours + ":"+ minutes + ":" + seconds;
-
-    if (hours == 10) {
-        document.getElementById("motivation").innerText = "Its time for Breakfast";
-        
-    }
-    if (hours == 14) {
-        document.getElementById("motivation").innerText = "Its time for Lunch";
-        
-    }
-    if (hours == 20) {
-        document.getElementById("motivation").innerText = "Its time for Dinner";
-        
-    }
-
-    document.getElementById("clock-date").innerText = FullDate;
-    document.getElementById("clock-time").innerText = fullTime;
-    console.log(hours);
-    // var Day = currentDateTime.getDate();
-    // console.log(Day);
-}
 
